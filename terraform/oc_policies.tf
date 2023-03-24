72a9db4c1dd79dbac550e307899acda0b35b35c2
kumargejara <kumar.gejara@gmail.com>
2023-03-23 22:29:39 -0500

    Update oc_policies.tf

--- terraform/oc_policies.tf
+++ terraform/oc_policies.tf
@@ -1,7 +1,17 @@
 resource "oci_identity_policy" "test_policy" {
-    #Required
     compartment_id = var.tenancy_ocid
     description = var.policy_description
     name = var.policy_name
     statements = var.policy_statements
+}
+
+data "oci_identity_policies" "test_policies" {
+    compartment_id = var.tenancy_ocid
+    name = var.policy_name
+}
+
+resource "oci_objectstorage_bucket" "test_bucket" {
+    compartment_id = "ocid1.compartment.oc1..aaaaaaaaoorr4zezczh4zsq2kcngjbeic7d66eh5xfifkid2gth4kcxwirra"
+    name = "my_sample_test_bucket123999"
+    namespace = "idlbk5tfnj1l"
 }

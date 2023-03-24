78921f2f1cc1b6c824778dfed99932d999a05a51
kumargejara <kumar.gejara@gmail.com>
2023-03-23 21:20:14 -0500

    Add files via upload

---
+++ terraform/get-oci-policy-data/get_policies_data.tf
@@ -0,0 +1,50 @@
+data "oci_identity_policies" "test_policies" {
+    #Required
+    compartment_id = var.tenancy_ocid
+
+    #Optional
+    name = var.policy_name
+}
+
+variable region { 
+    type    = string
+    default = "us-ashburn-1" 
+}
+
+variable compartment_ocid { 
+    type    = string
+    default = "ocid1.tenancy.oc1..aaaaaaaad3m5voewraokiw5gede3t3ahaz3zsuy7bs5iqecxujvedfzv3cea" 
+}
+
+variable tenancy_ocid {
+    type    = string
+    default = "ocid1.tenancy.oc1..aaaaaaaad3m5voewraokiw5gede3t3ahaz3zsuy7bs5iqecxujvedfzv3cea"
+}
+
+variable policy_name {
+    type    = string
+    default = "oci_developer_policies"
+}
+
+variable policy_statements {
+    type    = list(string)
+    default = ["test", "test123"]
+}
+
+output "policy_statements" {
+  description = "oci policies list"
+  value       = data.oci_identity_policies.itest_policies.statements
+}
+
+provider oci {
+	region = var.region
+}
+
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">= 4.103.0"
+    }
+  }
+}

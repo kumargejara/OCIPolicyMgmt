0fc5fb81e53ad0bd9be6390083423ee4164d123e
kumargejara <kumar.gejara@gmail.com>
2023-04-02 00:46:08 -0500

    Delete oc_policies.tf

--- terraform/oc_policies.tf
+++
@@ -1,9 +0,0 @@
-resource "oci_identity_policy" "test_policy" {
-
-    count = length(var.policy_sets)
-
-    compartment_id = var.tenancy_ocid
-    name = var.policy_sets[count.index].policy_name
-    description = var.policy_sets[count.index].policy_description
-    statements = var.policy_sets[count.index].policy_statements
-}

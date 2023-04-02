a64b19351aac32ca0a3e453f5431e8c6aa352329
kumargejara <kumar.gejara@gmail.com>
2023-04-02 00:45:57 -0500

    Add files via upload

--- terraform/oci_policies.tf
+++ terraform/oci_policies.tf
@@ -1,16 +1,9 @@
 resource "oci_identity_policy" "test_policy" {
-    compartment_id = var.tenancy_ocid
-    description = var.policy_description
-    name = var.policy_name
-    statements = var.policy_statements
-}
 
-data "oci_identity_policies" "test_policies" {
-    compartment_id = var.tenancy_ocid
-    name = var.policy_name
-}
+    count = length(var.policy_sets)
 
-output "policy_list" {
-  description = "oci policies list"
-  value       = data.oci_identity_policies.test_policies.policies
+    compartment_id = var.tenancy_ocid
+    name = var.policy_sets[count.index].policy_name
+    description = var.policy_sets[count.index].policy_description
+    statements = var.policy_sets[count.index].policy_statements
 }
